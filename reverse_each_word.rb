4c6ff4490c25f314094cde078fb6dbb2107c30c4
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-27 19:51:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,13 @@
  def reverse_each_word(sentence)
+reversed = []
 
-  reversed = []
-
-    sentence.split.collect do |word|
-      reversed << word.reverse
+  sentence.split.collect do |word|
+    i = 0
+    while i < word.length
+      reversed << word[i].to_s + " "
+      i -= 1
     end
+  end
 
   return reversed.join(' ')
 end
